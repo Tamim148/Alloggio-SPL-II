253c62a2a7e6ef4b342791bf5b391478108758ed
Your Name~ <tamimafnan308@gmail.com>
2023-03-08 14:44:47 +0600

    Technically not the first commit
    The frontend part of Home page is finished , the models halfway through finish

---
+++ Allegio/api/index.js
@@ -0,0 +1,47 @@
+import dotnev from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+
+import cookieParser from "cookie-parser";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/room.js";
+import usersRoute from "./routes/users.js";
+const app=express()
+dotnev.config()
+
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+
+app.use("/api/auth",authRoute)
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
+app.listen(8800, () => {
+    connect();
+    console.log("Connected to backend! made some change!!");
+  });
