7f938246aa38fb00f56b64d1e50cba00b666ad78
Your Name~ <tamimafnan308@gmail.com>
2023-03-11 15:16:18 +0600

    created a custom hook and fetched data for count_by_city

--- Allegio/client/src/components/featured/Featured.jsx
+++ Allegio/client/src/components/featured/Featured.jsx
@@ -1,42 +1,53 @@
+import useFetch from "../../hooks/useFetch";
 import "./fetured.css";
 
 const Featured = () => {
+  const { data, loading } = useFetch(
+    "http://localhost:8800/api/hotels/countByCity?cities=coxsbazar,rangamati,bandarban"
+  );
+  console.log(data);
   return (
     <div className="featured">
-      <div className="featuredItem">
-        <img
-          src="https://q-xx.bstatic.com/xdata/images/city/300x240/666853.jpg?k=b2212159e829c00b8b19af72b6211b25325d70811829e2eb4ef1045f8a568be5&o="
-          alt=""
-          className="featuredImg"
-        />
-        <div className="featuredTitles">
-          <h1>Coxs Bazar</h1>
-          <h2>123 properties</h2>
-        </div>
-      </div>
+      {loading ? (
+        "Loading please wait"
+      ) : (
+        <>
+          <div className="featuredItem">
+            <img
+              src="https://q-xx.bstatic.com/xdata/images/city/300x240/666853.jpg?k=b2212159e829c00b8b19af72b6211b25325d70811829e2eb4ef1045f8a568be5&o="
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Coxs Bazar</h1>
+              <h2>{data[0]} properties</h2>
+            </div>
+          </div>
 
-      <div className="featuredItem">
-        <img
-          src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/16/5e/59/rangamati-hanging-bridge.jpg?w=700&h=500&s=1"
-          alt=""
-          className="featuredImg"
-        />
-        <div className="featuredTitles">
-          <h1>Rangamati</h1>
-          <h2>123 properties</h2>
-        </div>
-      </div>
-      <div className="featuredItem">
-        <img
-          src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/77/22/14/amiakhum.jpg?w=700&h=500&s=1"
-          alt=""
-          className="featuredImg"
-        />
-        <div className="featuredTitles">
-          <h1>Bandarban</h1>
-          <h2>132 properties</h2>
-        </div>
-      </div>
+          <div className="featuredItem">
+            <img
+              src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/16/5e/59/rangamati-hanging-bridge.jpg?w=700&h=500&s=1"
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Rangamati</h1>
+              <h2>{data[1]} properties</h2>
+            </div>
+          </div>
+          <div className="featuredItem">
+            <img
+              src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/77/22/14/amiakhum.jpg?w=700&h=500&s=1"
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Bandarban</h1>
+              <h2>{data[2]} properties</h2>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
---
+++ Allegio/client/src/hooks/useFetch.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+const useFetch =(url) =>{
+  const [data, setdata]=useState([])
+  const [loading,setloading]=useState([false])
+  const [error,seterror]=useState([false])
+
+    useEffect( ()=>{
+      const fetchData = async () => {
+        setloading(true);
+        try {
+          const res = await axios.get(url);
+          setdata(res.data);
+        } catch (err) {
+          seterror(err);
+        }
+        setloading(false);
+      };
+      fetchData();
+    }, [url]); const reFetch = async () => {
+      setloading(true);
+      try {
+        const res = await axios.get(url);
+        setdata(res.data);
+      } catch (err) {
+        seterror(err);
+      }
+      setloading(false);
+    };
+  
+    return { data, loading, error, reFetch };
+  };
+  
+  export default useFetch;
