22487a7e741c2f740ac28125fcb249490e8a551e
Your Name~ <tamimafnan308@gmail.com>
2023-03-12 14:53:46 +0600

    Single hotel pages are done (Adon did it)

--- Allegio/api/controllers/hotel.js
+++ Allegio/api/controllers/hotel.js
@@ -40,6 +40,7 @@ export const createHotel = async (req, res, next) => {
   };
   export const getHotels = async (req, res, next) => {
     const { min, max, ...others } = req.query;
+    
     try {
       const hotels = await Hotel.find({
         ...others,
